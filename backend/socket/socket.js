a0903a4fed96284f121aa01ef3a256b5b201a733
rajeshpekala <pekalarajesh001@gmail.com>
2024-07-11 04:08:46 +0530

    Changed urls

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: [
-      "http://localhost:3000" || "https://chat-application-toop.onrender.com",
+      "http://localhost:3000" , "https://chat-application-toop.onrender.com",
     ],
     methods: ["GET", "POST"],
   },
