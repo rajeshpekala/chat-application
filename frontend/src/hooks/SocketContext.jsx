7ba668d05a423b7d068552aba42f7279dfaeff90
rajeshpekala <pekalarajesh001@gmail.com>
2024-07-11 03:26:17 +0530

    changed socket url

--- frontend/src/hooks/SocketContext.jsx
+++ frontend/src/hooks/SocketContext.jsx
@@ -16,7 +16,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://chat-application-toop.onrender.com/", {
+      const socket = io("https://chat-application-toop.onrender.com", {
         query: {
           userId: authUser._id,
         },
