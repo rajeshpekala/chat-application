3d91a91053cb1a3f5ea515984925b184367eacf6
rajeshpekala <pekalarajesh001@gmail.com>
2024-07-16 23:58:37 +0530

    changed routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
-import { BrowserRouter as Router,Route, Routes,Navigate } from "react-router-dom";
+import { Route, Routes,Navigate } from "react-router-dom";
 import Home from "./pages/home/Home";
 import SignUp from "./pages/signup/Signup";
 import Login from "./pages/login/Login";
 import {Toaster} from "react-hot-toast"
 import { useAuthContext } from "./context/AuthContext";
-import Header from "../src/pages/starterBody/Header"
-import Starter from "./pages/starterBody/Starter"
+
 
 
 
@@ -15,29 +14,21 @@ function App() {
 
   return (
     <div className="p-4 h-screen flex items-center justify-center">
-      
-       
-
-        <Routes>
-          <Route
-            path="/"
-            element={<Starter />}
-          />
-          <Route
-            path="/chathome"
-            element={authUser ? <Home /> : <Navigate to={"/login"} />}
-          />
-          <Route
-            path="/login"
-            element={authUser ? <Navigate to="/chathome" /> : <Login />}
-          />
-          <Route
-            path="/signup"
-            element={authUser ? <Navigate to="/chathome" /> : <SignUp />}
-          />
-        </Routes>
-        <Toaster />
-     
+      <Routes>
+        <Route
+          path="/"
+          element={authUser ? <Home /> : <Navigate to={"/login"} />}
+        />
+        <Route
+          path="/login"
+          element={authUser ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={authUser ? <Navigate to="/" /> : <SignUp />}
+        />
+      </Routes>
+      <Toaster />
     </div>
   );
  
