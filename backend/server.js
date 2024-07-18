2e41b3ebd509f36eb7eec5eaab2974b63ddd9dfe
rajeshpekala <pekalarajesh001@gmail.com>
2024-07-18 23:57:45 +0530

    commit2

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
-import authRoutes from "./routes/auth.routes.js";
+import authRoutes from "./routes/authroutes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 
